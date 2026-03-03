65a3aced1e7ecc33fe55c848257d0ec214bb5d41
Toks <taborg@TABORGs-MacBook-Pro.local>
2026-03-03 13:20:01 -0500

    instance added

--- main.tf
+++ main.tf
@@ -66,19 +66,25 @@ resource "aws_security_group" "terraform_demo_sg" {
   }
 }
 
-# resource "aws_instance" "terraform_demo_instance" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t3.micro"
-#   subnet_id     = aws_subnet.terraform_demo_subnet.id
-#   security_groups = [aws_security_group.terraform_demo_sg.name]
-
-#   tags = {
-#     Name = "terraform-demo-instance"
-#   }
-# }
-
 resource "aws_key_pair" "deploy_key" {
   key_name   = "terraform_demo_key"
   public_key = file("~/SSH/terraform_demo_key.pub")
   
+}
+
+resource "aws_instance" "terraform_demo_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.terraform_demo_subnet.id
+  security_groups = [aws_security_group.terraform_demo_sg.name]
+
+  tags = {
+    Name = "terraform-demo-instance"
+  }
+
+  key_name = aws_key_pair.deploy_key.key_name
+
+  root_block_device {
+    volume_size = 10
+  }
 }
