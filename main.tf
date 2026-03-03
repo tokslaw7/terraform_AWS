4c9bcfce3ee41d096e6141e8d638ec2243e84660
Toks <taborg@TABORGs-MacBook-Pro.local>
2026-03-03 12:16:23 -0500

    route table and default route added

--- main.tf
+++ main.tf
@@ -27,3 +27,16 @@ resource "aws_internet_gateway" "terraform_demo_igw" {
   }
 }
 
+resource "aws_route_table" "terraform_demo_route_table" {
+  vpc_id = aws_vpc.terraform_demo_vpc.id
+
+  tags = {
+    Name = "terraform-demo-route-table"
+  }
+}
+
+resource "aws_route" "terraform_demo_route" {
+  route_table_id         = aws_route_table.terraform_demo_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.terraform_demo_igw.id
+}
