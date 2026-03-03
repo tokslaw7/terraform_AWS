604fcc9b7586acc8495b6f531cbfbd9f021ac102
Toks <taborg@TABORGs-MacBook-Pro.local>
2026-03-03 13:09:03 -0500

    ami images, data sources and key pair added

--- main.tf
+++ main.tf
@@ -65,3 +65,20 @@ resource "aws_security_group" "terraform_demo_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# resource "aws_instance" "terraform_demo_instance" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t3.micro"
+#   subnet_id     = aws_subnet.terraform_demo_subnet.id
+#   security_groups = [aws_security_group.terraform_demo_sg.name]
+
+#   tags = {
+#     Name = "terraform-demo-instance"
+#   }
+# }
+
+resource "aws_key_pair" "deploy_key" {
+  key_name   = "terraform_demo_key"
+  public_key = file("~/SSH/terraform_demo_key.pub")
+  
+}
